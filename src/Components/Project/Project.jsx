68a1adbb214ee2bfb542cc595e9ec7e2f08d84dc
varunwalia09 <walia3616@gmail.com>
2025-05-31 11:22:20 +0530

    this is the saturday commit error solve

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,70 +1,72 @@
-import React, { useEffect, useRef, useState } from "react";
-import "./project.css";
-import Lenis from "@studio-freight/lenis";
-import { useTransform, useScroll, motion } from "framer-motion";
 
-const images = Array(12).fill("code2.jpg");
 
-const Column = ({ images, y }) => {
-  return (
-    <motion.div className="column" style={{ y }}>
-      {images.map((src, i) => (
-        <div key={i} className="imageContainer">
-          <img src={`/images/${src}`} alt="image" />
-        </div>
-      ))}
-    </motion.div>
-  );
-};
+// import React, { useEffect, useRef, useState } from "react";
+// import "./project.css";
+// import Lenis from "@studio-freight/lenis";
+// import { useTransform, useScroll, motion } from "framer-motion";
 
-export default function Project() {
-  const gallery = useRef(null);
-  const [dimension, setDimension] = useState({ width: 0, height: 0 });
+// const images = Array(12).fill("code2.jpg");
 
-  const { scrollYProgress } = useScroll({
-    target: gallery,
-    offset: ["start end", "end start"],
-  });
+// const Column = ({ images, y }) => {
+//   return (
+//     <motion.div className="column" style={{ y }}>
+//       {images.map((src, i) => (
+//         <div key={i} className="imageContainer">
+//           <img src={`/images/${src}`} alt="image" />
+//         </div>
+//       ))}
+//     </motion.div>
+//   );
+// };
 
-  const { height } = dimension;
+// export default function Project() {
+//   const gallery = useRef(null);
+//   const [dimension, setDimension] = useState({ width: 0, height: 0 });
 
-  const y = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
-  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
-  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
-  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3]);
+//   const { scrollYProgress } = useScroll({
+//     target: gallery,
+//     offset: ["start end", "end start"],
+//   });
 
-  useEffect(() => {
-    const lenis = new Lenis();
+//   const { height } = dimension;
 
-    const raf = (time) => {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    };
+//   const y = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
+//   const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
+//   const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
+//   const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3]);
 
-    const resize = () => {
-      setDimension({ width: window.innerWidth, height: window.innerHeight });
-    };
+//   useEffect(() => {
+//     const lenis = new Lenis();
 
-    window.addEventListener("resize", resize);
-    requestAnimationFrame(raf);
-    resize();
+//     const raf = (time) => {
+//       lenis.raf(time);
+//       requestAnimationFrame(raf);
+//     };
 
-    return () => {
-      window.removeEventListener("resize", resize);
-      lenis.destroy();
-    };
-  }, []);
+//     const resize = () => {
+//       setDimension({ width: window.innerWidth, height: window.innerHeight });
+//     };
 
-  return (
-    <main>
-      <div className="spacer"></div>
-      <div ref={gallery} className="gallery">
-        <Column images={[images[0], images[1], images[2]]} y={y} />
-        <Column images={[images[3], images[4], images[5]]} y={y2} />
-        <Column images={[images[6], images[7], images[8]]} y={y3} />
-        <Column images={[images[9], images[10], images[11]]} y={y4} />
-      </div>
-      <div className="spacer"></div>
-    </main>
-  );
-}
+//     window.addEventListener("resize", resize);
+//     requestAnimationFrame(raf);
+//     resize();
+
+//     return () => {
+//       window.removeEventListener("resize", resize);
+//       lenis.destroy();
+//     };
+//   }, []);
+
+//   return (
+//     <main>
+//       <div className="spacer"></div>
+//       <div ref={gallery} className="gallery">
+//         <Column images={[images[0], images[1], images[2]]} y={y} />
+//         <Column images={[images[3], images[4], images[5]]} y={y2} />
+//         <Column images={[images[6], images[7], images[8]]} y={y3} />
+//         <Column images={[images[9], images[10], images[11]]} y={y4} />
+//       </div>
+//       <div className="spacer"></div>
+//     </main>
+//   );
+// }
